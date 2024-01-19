95229a2810c71b0bc17a867aca13ea16103b609b
norahkibathi <norah.njeri@student.moringaschool.com>
2024-01-19 04:37:12 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+
+  // returnLastTwoDrivers 
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+
+  // array of functions 
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+  // FareMultiplier
+  const createFareMultiplier = function(integer) {
+    return function(fare) {
+      return fare * integer;
+    };
+  };
+
+  // fareDoubler 
+  const fareDoubler = createFareMultiplier(2);
+
+  // fareTripler
+  const fareTripler = createFareMultiplier(3);
+
+  // selectDifferentDrivers 
+  const selectDifferentDrivers = function(drivers, fctn) {
+    return fctn(drivers);
+  };
